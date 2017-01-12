93a182217992c6e8fbc524fd3b6126b7ce0294f4
Aliwa1205 <alinapetryshche@gmail.com>
2017-01-12 14:16:11 +0200

    task02 end

--- src/com/javarush/test/level03/lesson08/task02/Solution.java
+++ src/com/javarush/test/level03/lesson08/task02/Solution.java
@@ -13,6 +13,16 @@ public class Solution
     public static void main(String[] args) throws Exception
     {
 
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
+        String name = bufferedReader.readLine();
+
+        String snumb1 = bufferedReader.readLine();
+        int inumb1 = Integer.parseInt(snumb1);
+
+        String snumb2 = bufferedReader.readLine();
+        int inumb2 = Integer.parseInt(snumb2);
+
+        System.out.println(name + " получает " + inumb1 + " через " + inumb2 + " лет.");
     }
 }
